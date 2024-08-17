8951dcec034c117dbfe6f11efedab74b406bc368
SArvilla <sarvillamendez@gmail.com>
2024-08-17 10:44:34 -0500

    Fondo

--- Assets/2. Escenas/Paralax/Script_Paralax/FondoMovimiento.cs
+++ Assets/2. Escenas/Paralax/Script_Paralax/FondoMovimiento.cs
@@ -7,16 +7,14 @@ public class FondoMovimiento : MonoBehaviour
     [SerializeField] private Vector2 velocidadMovimiento;
     private Vector2 offset;
     private Material material;
-    private Rigidbody rb;
 
     private void Awake()
     {
         material = GetComponent<SpriteRenderer>().material;
-        rb = GameObject.FindGameObjectWithTag("Jugador").GetComponent<Rigidbody>();
     }
     void Update()
     {
-        offset = (rb.velocity.x * 0.1f) * velocidadMovimiento * Time.deltaTime;
+        offset = velocidadMovimiento * Time.deltaTime;
         material.mainTextureOffset += offset;
     }
 }
