8866bb5fe9c76b546280bc6f9236935cf9416404
luisazarama <lulu-zaram@hotmail.com>
2024-08-19 21:24:10 -0500

    Integracion wwise
    Integración sonidos caída puente, caída bloques, slider reloj

--- Assets/SonidoSliderTiempo.cs
+++ Assets/SonidoSliderTiempo.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class SonidoSliderTiempo : MonoBehaviour
+public class SonidoSliderTiempo : MonoBehaviour, IDragHandler
+
 {
+    [SerializeField] private UnityEvent<PointerEventData> onDragEvent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,15 @@ public class SonidoSliderTiempo : MonoBehaviour
     {
         
     }
+
+     public void OnDrag(PointerEventData eventData)
+    {
+        if (onDragEvent != null)
+        {
+            onDragEvent.Invoke(eventData);
+        }
+    }
+
+
 }
+
